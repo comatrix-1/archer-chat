f9012c05eaaf40a6121d4c9326bfcfc55d2373b6
comatrix <comatrix70@gmail.com>
2025-08-17 12:08:56 +0800

    remvoe unused auth package

--- packages/auth/server/types/context.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestMetadata } from '@project/lib/universal/extract-request-metadata';
-
-export type HonoAuthContext = {
-    Variables: {
-        requestMetadata: RequestMetadata;
-    };
-};
