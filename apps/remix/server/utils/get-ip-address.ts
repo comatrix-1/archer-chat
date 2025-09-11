42689cb5f5017adda501c996f81a6191ef32b580
comatrix <comatrix70@gmail.com>
2025-09-11 17:17:57 +0800

    remove unused server files

--- apps/remix/server/utils/get-ip-address.ts
+++
@@ -1,4 +0,0 @@
-export const getIpAddress = (req: Request) => {
-    const forwarded = req.headers.get('x-forwarded-for');
-    return forwarded ? forwarded.split(',')[0] : '127.0.0.1';
-};
--- apps/remix/server/utils/security.ts
+++
@@ -1,3 +0,0 @@
-export const generateUUID = () =>
-  crypto.randomUUID?.() ||
-  Date.now().toString(36) + Math.random().toString(36).substr(2);
